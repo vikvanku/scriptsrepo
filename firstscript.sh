17a412c5f90f9c0f5c732e6718dc49871cfa5e90
vikvanku <vikrantvankudre7@gmail.com>
2023-05-12 06:22:40 +0000

    script commit

---
+++ firstscript.sh
@@ -0,0 +1,11 @@
+echo "**************************Start of script************************"
+mkdir folder1
+cd folder1
+touch file1.txt > "this is first file"
+touch file2.txt > "this is second file"
+echo "list all the files"
+ls
+echo "the file content is"
+cat file1.txt
+cat file2.txt
+echo "**********************end of script***********"
